7f8c6e4a7f16174577d8f19930046823d03b3f21
이유진 <eujjin@naver.com>
2021-07-26 21:33:24 +0900

    '0726_algo_ing'

--- 백준/BJ_2467_용액.py
+++ 백준/BJ_2467_용액.py
@@ -1,27 +1,48 @@
+def find_pair(num):
+    global min_sum, pair
+    left, right = 0, N-1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        # 나와 pair를 할 수 없다. 즉 수치가 똑같은 용액과 pair를 지어줄 수 없다.
+        if num == liquid_list[mid]:
+            if mid == 0 or mid == N-1:
+                break
+            if abs(num + liquid_list[mid-1]) <= abs(num + liquid_list[mid+1]):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        else:
+            if abs(liquid_list[mid] + num) < abs(min_sum):
+                min_sum = liquid_list[mid] + num
+                pair = [liquid_list[mid], num]
+
+                if min_sum == 0:   ## 최소 합이 0 이라면 바로 break 하기
+                    break
+
+            if liquid_list[mid] + num < 0:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+
 
 N = int(input())
-liquid_list = []
-trans = -1
-for i in range(N):
-    num = int(input())
-    if trans == -1 and num > 0:
-        trans = i
-    liquid_list.append(num)
+liquid_list = list(map(int, input().split()))
 
-min_sum = 0
-comb = []
-if abs(liquid_list[trans-1] + liquid_list[trans-2]) < abs(liquid_list[trans] + liquid_list[trans+1]):
-    min_sum = abs(liquid_list[trans-1] + liquid_list[trans-2])
-    comb = [trans-1, trans-2]
-else:
-    min_sum = abs(liquid_list[trans] + liquid_list[trans+1])
-    comb = [trans, trans+1]
+liquid_list.sort()
 
-acid_left, acid_right = 0, trans - 1
-kal_left, kal_right = trans, len(liquid_list) - 1
+min_sum = 2000000000
+pair = [0, 0]
 
-while acid_left <= acid_right and kal_left <= kal_right:
-    pass
+for num in liquid_list:
+    find_pair(num)
 
+    if min_sum == 0:
+        break
 
-
+pair.sort()
+print(*pair)
