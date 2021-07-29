cb10494f011fb96b5d0d89c8fe1b143374c583ac
이유진 <eujjin@naver.com>
2021-07-29 21:21:18 +0900

    0729_algo_end

--- 백준/BJ_1780_종이의 개수.py
+++ 백준/BJ_1780_종이의 개수.py
@@ -1,4 +1,48 @@
+def check(r, c, size):
+    global minus_cnt, zero_cnt, plus_cnt
+    now = arr[r][c]
+
+    for i in range(r, r+size):
+        for j in range(c, c+size):
+            if arr[i][j] != now:
+                n_size = size // 3
+                check(r, c, n_size)
+                check(r, c + n_size, n_size)
+                check(r, c + n_size * 2, n_size)
+
+                check(r + n_size, c, n_size)
+                check(r + n_size, c + n_size, n_size)
+                check(r + n_size, c + n_size * 2, n_size)
+
+                check(r + n_size * 2, c, n_size)
+                check(r + n_size * 2, c + n_size, n_size)
+                check(r + n_size * 2, c + n_size * 2, n_size)
+
+                return
+
+    if now == -1:
+        minus_cnt += 1
+        return
+
+    elif now == 1:
+        plus_cnt += 1
+        return
+
+    else:
+        zero_cnt += 1
+        return
 
 
 
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
+minus_cnt = 0
+zero_cnt = 0
+plus_cnt = 0
+
+check(0, 0, N)
+
+print(minus_cnt)
+print(zero_cnt)
+print(plus_cnt)
