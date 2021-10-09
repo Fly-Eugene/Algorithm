28b4bc3ae649014ced61c6bff0e23a7c7e652822
이유진 <eujjin@naver.com>
2021-10-09 22:00:08 +0900

    BJ_5568, 문자열

--- 백준/BJ_5568_card.py
+++ 백준/BJ_5568_card.py
@@ -1,5 +1,28 @@
 
+def perm(idx):
+    if idx == k:
+        res.add(''.join(sel))
+        return
+    else:
+        for i in range(n):
+            if visited[i] == 0:
+                sel[idx] = cards[i]
+                visited[i] = 1
+                perm(idx+1)
+
+                visited[i] = 0
 
 
+n = int(input())
+k = int(input())
+cards = []
+res = set()
 
+for i in range(n):
+    cards.append(input())
 
+sel = [0]*k
+visited = [0]*n
+
+perm(0)
+print(len(res))
