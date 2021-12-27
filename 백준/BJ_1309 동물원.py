c2bb722b9bb474e1dd6cf7c25359d318fcfe3e51
이유진 <eujjin@naver.com>
2021-12-28 00:27:05 +0900

    BJ_1309 dp

---
+++ 백준/BJ_1309 동물원.py
@@ -0,0 +1,18 @@
+
+N = int(input())
+
+dp = [1 for _ in range(N+1)]
+dp[1] = 3
+
+if N == 1:
+    print(dp[1])
+else:
+    for i in range(2, N+1):
+        dp[i] = dp[i-2] + (dp[i-1]*2)
+        dp[i] %= 9901
+    print(dp[N])
+
+
+
+
+
