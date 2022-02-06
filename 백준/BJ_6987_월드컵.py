49c76027b763fccae42585f93db76cd08fbc6720
이유진 <eujjin@naver.com>
2022-02-06 22:03:16 +0900

    simulation gold5

--- 백준/BJ_6987_월드컵.py
+++ 백준/BJ_6987_월드컵.py
@@ -1,39 +1,41 @@
 
-def comb(i, j):
-    if i == 2:
-        comb_list.append(comb_res)
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+def play_game(round):
+    global res
+
+    if round == 15:
+        for i in range(6):
+            if sum(game_result[i]) != 0:
+                return
+        res = 1
         return
 
-    else:
-        for k in range(j, 6-2+i+1): ## range(j, n-r+i+1)
-            comb_res[i] = team[k]
-            comb(i+1, j+1)
+    n1, n2 = comb_list[round]
+    for r1, r2 in [(0, 2), (1, 1), (2, 0)]:
+        if game_result[n1][r1] > 0 and game_result[n2][r2] > 0:
+            game_result[n1][r1] -= 1
+            game_result[n2][r2] -= 1
+
+            play_game(round + 1)
+            game_result[n1][r1] += 1
+            game_result[n2][r2] += 1
 
 
-results = [[0, 0, 0] * 6 for _ in range(3)]
+comb_list = list(combinations(range(6), 2))
+for i in range(4):
+    game_input = list(map(int, input().split()))
+    game_result = [[0, 0, 0] for _ in range(6)]
+    res = 0
 
-team = [i for i in range(6)]
-comb_res = [0, 0]
-comb_list = []
-comb(0, 0)
+    for j in range(18):
+        gi = game_input[j]
+        game_result[j//3][j%3] = gi
 
-for comb_item in comb_list:
-    a, b = comb_item
-
-    # 무승부 일때
-    for i in range(3):
-        results[i][a * 3 + 1] += 1
-        results[i][b * 3 + 1] += 1
-
-    # a 승, b 패
-    for i in range(3):
-        results[i][a * 3] += 1
-        results[i][b * 3 + 2] += 1
-
-    # a 패, b 승
-    for i in range(3):
-        results[i][a * 3 + 2] += 1
-        results[i][b * 3] += 1
+    play_game(0)
+    print(res, end=' ')
 
 
 
